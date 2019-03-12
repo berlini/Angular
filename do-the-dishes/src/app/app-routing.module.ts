1c51dfa25c818aee9739dfe52c58de648a1a2c6b
Felipe Berlini <felipe.berlini@gmail.com>
2019-03-12 18:58:15 -0300

    DoTheDishes
    Login routing module + Register page

--- do-the-dishes/src/app/app-routing.module.ts
+++ do-the-dishes/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginPageComponent } from './login/login-page/login-page.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginPageComponent, pathMatch: 'full' },
   { path: '', pathMatch: 'full', redirectTo: 'login'}
 ];
 
--- do-the-dishes/src/app/login/login.module.ts
+++ do-the-dishes/src/app/login/login.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
   declarations: [
-    LoginPageComponent
+    LoginPageComponent,
+    RegisterPageComponent
   ],
   imports: [
-    CommonModule
-  ],
-  bootstrap: [LoginPageComponent]
+    CommonModule,
+    LoginRoutingModule
+  ]
 })
 export class LoginModule { }
